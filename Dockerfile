d1f471953454fc85d856c965e18dfc0401d74e31
Maria Mariotti <mmariotti@mmariotti-ltm.internal.salesforce.com>
2019-03-17 12:53:15 -0300

    Added download filter and fixed pagination bug.

--- Dockerfile
+++ Dockerfile
@@ -20,4 +20,5 @@ EXPOSE 80
 ENV BOOKS_API_TOKEN AIzaSyCOeFB-k532HjyVfsYJK9pKKx9UGDoqq5g
 
 # Run app.py when the container launches
+#CMD ["python", "src/main/app.py"]
 CMD ["/usr/local/bin/gunicorn", "src.main.wsgi"]
--- src/main/app.py
+++ src/main/app.py
@@ -15,6 +15,8 @@ SEARCH_PARAM = "key_words"
 PARAM_SORT = "sorted"
 PARAM_START_INDEX = "start_index"
 PARAM_MAX_RESULTS = "max_results"
+ADDITIONAL_FITLERS = {"filter" : "free-ebooks"}
+API_MAX_RESULTS = 40
 
 @application.route("/v1/books")
 @cross_origin()
@@ -23,13 +25,23 @@ def query_books():
         if (not request.args.has_key(SEARCH_PARAM)):
             return get_response({"message" : "Se debe ingresar algun parametro de busqueda valido!"}, status.HTTP_400_BAD_REQUEST)
 
-        query_param = "q={}".format(request.args.get(SEARCH_PARAM))
+        query_params = "q={}".format(request.args.get(SEARCH_PARAM))
 
-        response = requests.get(books_base_url + "?{}".format(query_param), headers = {"key" : os.environ['BOOKS_API_TOKEN']})
-        book_list = response.json()["items"] if (response.json()["totalItems"] > 0) else []
+        for param_name, value in ADDITIONAL_FITLERS.items():
+            query_params += "&{}={}".format(param_name, value)
+
+        book_list = []
+        i = 0
+        more_results = True
+        while(more_results):
+            query_url = books_base_url + "?maxResults={}&startIndex={}&{}".format(API_MAX_RESULTS, i, query_params)
+            response = requests.get(query_url, headers = {"key" : os.environ['BOOKS_API_TOKEN']})
+            aux_book_list = response.json()["items"] if (response.json().has_key("items") > 0) else []
+            more_results = len(aux_book_list) > 0
+            book_list += aux_book_list
+            i += API_MAX_RESULTS
+
         start_index = 0 if (not request.args.has_key(PARAM_START_INDEX)) else int(request.args[PARAM_START_INDEX])
-        print(len(book_list))
-
         if (start_index >= len(book_list)):
             return get_response({"items" : []}, status.HTTP_200_OK)
 
@@ -41,9 +53,9 @@ def query_books():
         
         return get_response({"items" : book_list[start_index : new_end_index]}, status.HTTP_200_OK)
     except ValueError as e:
-        return get_response({"message" : "La busqueda que intenta realizar no es valida! : {}".format(e.message)}, status.HTTP_400_BAD_REQUEST)
+        return get_response({"message" : "La busqueda que intenta realizar no es valida! : {}".format(str(e))}, status.HTTP_400_BAD_REQUEST)
     except Exception as e:
-        return get_response({"message" : "Se produjo un error inesperado: {}".format(e.message)}, status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return get_response({"message" : "Se produjo un error inesperado: {}".format(str(e))}, status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 def get_response(request_response_obj, response_status):
     return make_response(json.dumps(request_response_obj), response_status)
